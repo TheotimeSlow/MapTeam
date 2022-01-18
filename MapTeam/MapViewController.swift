1f8ea28092760374a8f685c51ef7c4799981ea88
TheotimeSlow <theoslow@yahoo.fr>
2022-01-18 14:58:35 +0100

    Ajout d'un pin et zoom sur la position actuelle

--- MapTeam/MapViewController.swift
+++ MapTeam/MapViewController.swift
@@ -31,6 +31,11 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         print(center)
         let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         map.setRegion(mRegion, animated: true)
+        
+        let mkAnnotation:MKPointAnnotation = MKPointAnnotation()
+        mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
+        mkAnnotation.title = self.setUsersClosestLocation(mLatitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
+        map.addAnnotation(mkAnnotation)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
@@ -47,5 +52,24 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             locationManager.startUpdatingLocation()
         }
     }
-    
+ 
+    func setUsersClosestLocation(mLatitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String{
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: mLatitude, longitude: mLongitude)
+        
+        geoCoder.reverseGeocodeLocation(location){
+            (placemarks, error)->Void in
+            
+            if let mPlacemark = placemarks{
+                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
+                    if let Name = dict["Name"] as? String{
+                        if let City = dict["City"] as? String{
+                            self.currentLocationStr = Name + ", " + City
+                        }
+                    }
+                }
+            }
+        }
+        return currentLocationStr
+    }
 }
